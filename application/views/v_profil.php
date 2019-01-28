ec07baf2a900d67bef4b2b20d4d2ccd32c3e812a
andhikapradana <andhika_pradana_26rpl@student.smktelkom-mlg.sch.id>
2019-01-28 14:23:39 +0700

    login crud

---
+++ application/views/v_profil.php
@@ -0,0 +1,54 @@
+	<div class="block-header">
+		<H1 style="margin-left: 10px; padding-top: 20px;">PROFILKU</H1>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="card">
+			<div class="row clearfix">
+				<div class="col-xs-12 col-sm-6" style="width: 100%;">
+					
+				</div>
+			</div>
+			<div class="body">
+			<div class="row">
+				<div class="col-md-4">
+					<img src="<?=base_url('assets/dhika.jpeg')?>" style="width:370px; height:190px; margin-left: 18px; margin-top: 30px;">
+				</div>
+				<div class="col-md-8">
+					<table class="table table-hover table-striped" style="font-size: 17px; margin-left: 70px;">
+						<tr>
+							<td>
+								Nama : Andhika Pradana Byantara Ferdiananda
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Kelas : XI-RPL 2
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Umur : 16 tahun
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Alamat : Jl Danau Ranau VIII
+							</td>
+						</tr>
+						<tr>
+							<td>
+								IG : abcdhika_
+							</td>
+						</tr>
+						<tr>
+							<td>
+								WA : 0822306memangnyasayapeduli
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			</div>
+		</div>
+
+	</div>
